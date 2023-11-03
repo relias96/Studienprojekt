8bb129f14b644db3ed2ec4a42a5a75995e5995e0
RobinElias <robin.elias.96@icloud.com>
2023-11-03 10:09:02 +0100

    redefinition of some funktions exclude fig and ax

--- src/HostParasitoid.jl
+++ src/HostParasitoid.jl
@@ -29,12 +29,12 @@ end
 
 
 "Plottet ein Bifurkationsdiagramm für unser Host-Parasitoid System"
-function plot_orbitdiagram(
+function plot_orbitdiagram!(
+    ax,
     system,
     x_range::StepRangeLen;
     params=[] ::Vector{Tuple{Int64, Float64}},  #Tuple Index and Value
-    init_vals= (0.5, 0.5) ::Tuple{Float64, Float64},
-    title = "")
+    init_vals= (0.5, 0.5) ::Tuple{Float64, Float64})
 
     for p in params
         set_parameter!(system, p[1], p[2])
@@ -44,14 +44,11 @@ function plot_orbitdiagram(
         set_state!(system, init_vals)
     end
 
-    fig::Figure = Figure(resolution=(2000,600))
-    ax = fig[1, 1] = Axis(fig, title=title, xticks=first(x_range):0.5:last(x_range))
-    o::Vector{Vector{Float64}} = orbitdiagram(system , 1, 2, x_range, Ttr= 20000, n=50, u0 = init_vals)
+    o::Vector{Vector{Float64}} = orbitdiagram(system , 1, 2, x_range, Ttr= 10000, n=500, u0=init_vals)
 
     for i  in zip(x_range, o::Vector{Vector{Float64}})
         scatter!(ax, fill(i[1],size(i[2])), i[2], markersize=1, color="black", dpi=300)
     end 
-    return fig
 end
 
 "Plottet eine Zeitreihe"
@@ -71,15 +68,16 @@ function plot_timeseries(
 end
 
 "Plottet den Statespace"
-function plot_statespace(
-    system=system,
+function plot_statespace!(
+    ax,
+    system,
     time_end=100;
     init_vals=(0.5,0.5))
-    data, time = trajectory(system, time_end,init_vals, Ttr=20000)
-    fig = Figure()
-    ax = Axis(fig[1, 1], xlabel = "Host", ylabel = "Parasitoid", title = "Statespace")
-    fig, ax, plo = scatter(data[:,1], data[:, 2])
-    return fig
+    data, time = trajectory(system, time_end,init_vals, Ttr=0)
+    #fig = Figure()
+    #ax = Axis(fig[1, 1], xlabel = "Host", ylabel = "Parasitoid", title = "Statespace")
+    scatter!(ax, data[:,1], data[:, 2], color="black")
+    #return fig
 end
 
 function generate_cmap(n)
@@ -90,8 +88,9 @@ function generate_cmap(n)
     end
 end
 
-function plot_basin(
-    system;
+function plot_basin!(
+    fig :: Figure,
+    system,
     xgrid=range(0.001,1;length=1000),
     ygrid=range(0.001,1;length=1000),
     t=""
@@ -99,16 +98,16 @@ function plot_basin(
 
     theme!(length(xgrid)/50)
     grid = (xgrid, ygrid)
-    mapper = AttractorsViaRecurrences(system, grid, sparse = true, Ttr= 0,consecutive_recurrences = 800, attractor_locate_steps = 1000)
-    basins, attractors = basins_of_attraction(mapper,grid)
-    fracs = basins_fractions(basins)
+    mapper = AttractorsViaRecurrences(system, grid, sparse = true, Ttr= 0,consecutive_recurrences = 1000, attractor_locate_steps = 10000)
+    basin, attractors = basins_of_attraction(mapper,grid)
+    fracs = basins_fractions(basin)
     #println(fracs)
     ###########
-    ids = sort!(unique(basins))
+    ids = sort!(unique(basin))
     # Modification in case attractor labels are not sequential:
     for i in 2:length(ids)
         if ids[i] - ids[i-1] ≠ 1
-            replace!(basins, ids[i] => ids[i-1]+1)
+            replace!(basin, ids[i] => ids[i-1]+1)
             replace!(ids, ids[i] => ids[i-1]+1)
         end
     end
@@ -117,14 +116,14 @@ function plot_basin(
 
  ###############   
 
-    bas = Figure(resolution=(length(xgrid),length(ygrid)))
-    ax = Axis(bas[1,1],
+    #bas = Figure(resolution=(length(xgrid),length(ygrid)))
+    ax = Axis(fig[1,1],
         xlabel="Host",
         ylabel="Parasitoid",
         title=t)
-    hm = heatmap!(ax,xgrid,ygrid,basins, colormap = cmap, colorrange = (ids[1] - 0.5, ids[end]+0.5),)
+    hm = heatmap!(ax,xgrid,ygrid,basin, colormap = cmap, colorrange = (ids[1] - 0.5, ids[end]+0.5),)
 
-    cb = Colorbar(bas[1,2], hm)
+    cb = Colorbar(fig[1,3], hm)
     l = string.(ids)
     if 0 ∈ ids
         l[1] = "-1"
@@ -134,18 +133,20 @@ function plot_basin(
     set_theme!()
     ####
 
-    statespace = Figure()
-    axis = Axis(statespace[1, 1],
+    #statespace = Figure()
+    axis = Axis(fig[1, 2],
+        limits = (first(xgrid), last(xgrid), first(ygrid), last(ygrid)),
         xlabel="Host",
         ylabel="Parasitoid",
         title="statespace matching the basin Plot",
     )
     for m in attractors
-        scatter!(axis, m[2][:,1], m[2][:,2], label="Attractor " * string(m[1]),color=COLORS[m[1]+1], markersize=5)
+        scatter!(axis, m[2][:,1], m[2][:,2], label="Attractor " * string(m[1]),color=COLORS[m[1]+1], markersize=8)
     end
     axislegend()
 
-    return bas, statespace, attractors
+    return ax, axis, basin
+    #return fig, statespace, attractors
 end
 
 function plot_statespace_Ntimes(
