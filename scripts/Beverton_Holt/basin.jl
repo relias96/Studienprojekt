ca5c8e5b2091b0b0e5482318cca805637e13ced6
RobinElias <robin.elias.96@icloud.com>
2023-11-20 12:10:39 +0100

    parameter changes

--- scripts/Beverton_Holt/basin.jl
+++ scripts/Beverton_Holt/basin.jl
@@ -5,30 +5,30 @@ using DrWatson
 include(srcdir("HostParasitoid.jl"))
 
 
-# λ, a, h, type 
-#for Lambda = 7
-#[19.7, 20, 20.5, 21.7, 22.75, 23.5, 25.5, 27]
 
-
-# For Lambda = 7,5
-[18.5, 19.0, 19.5, 20, 21.3, 21.9, 22.5, 24, 26, 26.8, 27, 27.7, 28.5]
-
-#[ 21, 21.5, 22 ,22.5, 23.5, 25, 26, 27]
-for a in [20]
-    parameter = [2, a, 2, 3]
+21.85
+for a in [21.85]
+    parameter = [7, a, 0.1, 3]
     population = [rand(0.0:0.01:2.0),rand(0.0:0.01:2.0)]
     system = DiscreteDynamicalSystem(Beverton_Holt, population, parameter)
 
-    xgrid=range(0.0001,1,1200)
-    ygrid=range(0.0001,1,1200)
+    xgrid=range(0.0001,1,1000)
+    ygrid=range(0.0001,1,1000)
 
 
-    fig = Figure(resolution=(2*length(xgrid),length(ygrid)))
-    ax, axis, basin = plot_basin!(fig, system, xgrid, ygrid , string(parameter))
+    fig = Figure(resolution=(2*length(xgrid),1*length(ygrid)))
 
-     plot_statespace!(ax, system, init_vals=(0.75,0.45))
+    data = get_basin(system)
+
+
+
+    ax, axis = plot_basin!(system, fig, data)
+
+    plot_statespace!(ax, system,10000, init_vals=(0.3,0.2))
+    plot_statespace!(ax, system,10000, init_vals=(0.31,0.21))
 
     display(fig)
 
-    save(plotsdir("BevertonHolt","basins_lambda=20" , "basin_a="*string(a)*".png"), fig)
+
+    #save(plotsdir("BevertonHolt","basins_lambda=20" , "basin_a="*string(a)*".png"), fig)
 end
