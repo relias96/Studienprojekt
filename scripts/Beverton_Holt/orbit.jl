c2033e5734b4c458d22ffe1af6340ae313afac29
RobinElias <robin.elias.96@icloud.com>
2023-11-20 12:11:17 +0100

    parameter changes

--- scripts/Beverton_Holt/orbit.jl
+++ scripts/Beverton_Holt/orbit.jl
@@ -5,17 +5,19 @@ include(srcdir("HostParasitoid.jl"))
 
 
 # λ, a, h, type
-λ = 20
-a = 20.0
-h = 2
+λ = 6.8
+α = 20.0
+μ = 0.1
 
-parameter = [λ, a, h , 3]
+
+parameter = [λ, α, μ , 3]
 population = [rand(0.2:0.0005:1.0),rand(0.2:0.01:1.0)]
 #population=[0.01,0.01]
 system = DiscreteDynamicalSystem(Beverton_Holt, population, parameter)
 
 
-x_range = 12:0.01:30
+x_range = 14.5:0.0005:15
+
 
 fig::Figure = Figure(resolution=(2000,600))
 ax = fig[1, 1] = Axis(fig, title="λ = " * string(λ)* " --- h = "*string(h), xticks=first(x_range):0.5:last(x_range))
